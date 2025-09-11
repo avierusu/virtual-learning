4df06cd2cb7a24b17d98f7037fea135d89592c0c
avierusu <avierusu@gmail.com>
2025-09-11 09:02:56 -0500

    Finished Writing comprehensions.py

--- harvard/cs50p-python/02_loops/comprehensions.py
+++ harvard/cs50p-python/02_loops/comprehensions.py
@@ -1,3 +1,6 @@
+import csv
+import re
+
 def main():
     counts = {}
     words = get_words("address.txt")
@@ -18,12 +21,45 @@ def main():
     
     save_counts(counts)
 
-# Implementation of get_words() and save_counts() not provided
-def get_words():
-    ...
+# Implementations of get_words and save_counts provided
+def get_words(file_name):
+    with open(file_name, "r") as f:
+        contents = f.read()
+    
+    contents = " ".join(contents.split())
+    contents = re.sub(r"[^\w\- ]", "", contents)
+    contents = re.sub(r"\-\-", " ", contents)
+    return contents.split()
 
-def save_counts():
-    ...
+def save_counts(counts):
+    with open("counts.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Word", "Count"])
+        for word, count in sorted(counts.items(), key=lambda x: x[1], reverse=True):
+            writer.writerow([word, count])
 
 
 main()
+
+'''
+import csv
+import re
+
+
+def get_words(filename):
+with open(filename, "r") as f:
+contents = f.read()
+
+contents = " ".join(contents.split())
+contents = re.sub(r"[^\w\- ]", "", contents)
+contents = re.sub(r"\-\-", " ", contents)
+return contents.split()
+
+
+def save_counts(counts):
+with open("counts.csv", "w") as f:
+writer = csv.writer(f)
+writer.writerow(["Word", "Count"])
+for word, count in sorted(counts.items(), key=lambda x: x[1], reverse=True):
+writer.writerow([word, count])
+'''
